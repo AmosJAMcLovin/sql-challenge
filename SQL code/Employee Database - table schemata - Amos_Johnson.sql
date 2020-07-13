6f0bcbc5578ec626181e9a09d67768f19578807a
Amos Johnson <amosrcj17@outlook.com>
2020-07-13 16:26:49 -0400

    add sql

---
+++ SQL code/Employee Database - table schemata - Amos_Johnson.sql
@@ -0,0 +1,36 @@
+CREATE TABLE departments(
+    dept_no VARCHAR,
+    dept_name VARCHAR
+);
+
+CREATE TABLE dept_emp(
+    emp_no INT,
+    dept_no VARCHAR,
+    dept_name VARCHAR
+);
+
+CREATE TABLE dept_manager(
+    dept_no VARCHAR,
+    emp_no INT,
+    dept_name VARCHAR
+);
+
+CREATE TABLE employees(
+    emp_no INT,
+    emp_title VARCHAR,
+    birth_date DATE,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex VARCHAR,
+    hire_date DATE
+);
+
+CREATE TABLE salaries(
+    emp_no INT,
+    salary INT
+);
+
+CREATE TABLE titles(
+    title_id VARCHAR,
+    title VARCHAR
+);
